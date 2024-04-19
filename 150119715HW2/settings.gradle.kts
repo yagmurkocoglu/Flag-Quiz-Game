6550c80e76bc5e83504ca4d303b1bb457397ed16
Yağmur Koçoğlu <yagmurkcgl@outlook.com>
2024-04-19 15:31:51 +0300

    add mobileProject2

---
+++ 150119715HW2/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "150119715HW2"
+include(":app")
